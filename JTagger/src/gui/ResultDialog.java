41451b697e3451b9b6e76e691af5fbf934e6bc7b
michele <micheleroviello@gmail.com>
2014-12-31 12:37:20 +0100

    Rimozione warning

--- JTagger/src/gui/ResultDialog.java
+++ JTagger/src/gui/ResultDialog.java
@@ -4,7 +4,6 @@ import java.awt.Desktop;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.net.Proxy;
 import java.net.URI;
